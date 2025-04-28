d0d9cf5fd97a6911f4a43a35bc4932477ce95d22
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-04-29 00:46:32 +0530

    feat: Add Home component with welcome message and layout for just

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+      <div className="container mx-auto p-4">
+        <h1 className="text-3xl font-bold">Welcome to Technest</h1>
+        <p>This is the homepage. Posts and community questions will be displayed here.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
