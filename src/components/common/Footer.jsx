f3e7a0ba31bb844b060df3c31ebb57363f8f0dbd
Dhanithya Beligolla <it22085580@my.sliit.lk>
2025-05-02 13:43:19 +0530

    feat: add Navbar and Footer components for site navigation and footer information

---
+++ src/components/common/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-900 p-4 text-center text-gray-400">
+      © 2025 TechNest. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
