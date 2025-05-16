8712adf501f68d369fbd9e08b9268a421d50730f
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-05-16 08:55:49 +0530

    fix: update navigation path after login to point to post management dashboard
    change the after ogig navifgation

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -22,7 +22,7 @@ function Login({ role }) {
         showConfirmButton: false,
         timer: 1500
       });
-      navigate('/PostDashboardPage');    // Redirect to the post dashboard page
+      navigate('/postManagement/PostDashboardPage');    // Redirect to the post dashboard page after login
     } else {
       Swal.fire({
         title: "Invalid credentials",
