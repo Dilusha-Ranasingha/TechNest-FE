f941385b2c5f946a0a6a35efdf889edde8af3c05
Dilusha-Ranasingha <dilushar10@gmail.com>
2025-05-16 18:53:22 +0530

    feat: add 'Manage My Posts' button to Navbar for user post management
    add a new navigation to  the maneg the post

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -52,6 +52,7 @@ function Navbar() {
               <>
                 <Link to="/dashboard" className="text-white hover:text-cyan-400">Dashboard</Link> {/* Updated */}
                 <Link to="/tutorials" className="text-white hover:text-cyan-400">Tutorials</Link>
+                <Link to="/postManagement/user-posts" className="text-white hover:text-cyan-400">Manage My Posts</Link> {/* add a new button to mange the own post */}
                 <Link to="/advertisements" className="text-white hover:text-cyan-400">Advertisement</Link>
                 <Link to="/feed" className="text-white hover:text-cyan-400">Feed</Link>
                 <Link to="/profile" className="flex items-center">
